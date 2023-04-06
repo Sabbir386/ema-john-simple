b4b689356b38523e4e3665654d8a06e50e7bd348
Sabbir386 <sabbir386.sa@gmail.com>
2023-04-06 22:30:58 +0600

    added cart delete optiopn

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import ReviewItem from '../reviewItem/reviewItem';
 import './Orders.css'
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 
 const Orders = () => {
-    const cart = useLoaderData();
-    console.log(cart);
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
     return (
         <div className='shop-container'>
             <div className="review-container">
                 {
-                    cart.map(product => <ReviewItem
+                    savedCart.map(product => <ReviewItem
                         key={product.id}
                         product={product}
-
+                        handleRemoveFromCart={handleRemoveFromCart}
                     ></ReviewItem>)
 
                 }
